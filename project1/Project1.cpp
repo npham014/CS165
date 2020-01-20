c91422435693e2c287651840f45afa7931ea55d6
Raymond <rchle001@ucr.edu>
2020-01-20 15:24:37 -0800

    still working on it

--- project1/Project1.cpp
+++ project1/Project1.cpp
@@ -5,6 +5,7 @@
 //note: to compile:  g++ -I../hashlib2plus/trunk/src/ -std=c++11 ../hashlib2plus/trunk/src/hl_md5wrapper.cpp ../hashlib2plus/trunk/src/hl_md5.cpp Project1.cpp
 std::string convertHash(std::string hash, int length);
 std::string appInt(std::string pass, int length);
+std::string print64(std::string base, unsigned char inc[], int incsize);
 
 
 int main(int argc, char** argv) {
@@ -43,18 +44,51 @@ int main(int argc, char** argv) {
 		if((j%7) > 0) intermiprehash += password;
 		intermiprehash += (j%2) == 0 ? password : intermi;
 		intermi = myWrapper->getHashFromString(intermiprehash);
-		//if(intermi == ff202f2e9b6ac6e495570536fc89fd2a)
 	}
 	std::cout << "intermediate1000: " << intermi << std::endl;
-	std::string s = "";
-	unsigned char x[22] = {0};
-	for(unsigned int i = 0; i < intermi.size(); i++)
+	std::string s = convertHash(intermi, 16);
+	std::cout << "s: " << s << "\ns size: " << s.size() <<  std::endl;
+	/*unsigned char x[22];
+	x[0] = s.at(0) & 0x3f;
+	//x[0] = x << 6;
+	unsigned char j = 1;
+     	for(unsigned int i = 0; i < 16; i++)
 	{
-		unsigned char temp = intermi.at(i);
-		x[i] = temp & 0x3f;
-	}
+		x[j] += (s.at(i) >> 6) + (s.at(i+1) << 2);
+  	     	//x[j] = x << 6;
+	}*/
+
+	/*unsigned char x[16];
+	for(unsigned char i = 0; i < 16; i++)
+	{
+		x[i] = (s.at(i)) & 0x3f;
+	}*/
+	/*unsigned long int x = s.at(s.size()-1) & 0x3f;
+	x << 6;
+	for(unsigned char i = 15; i >0; i--)
+	{
+		
+	}*/
+	std::string y; y += s.at(11);y += s.at(4); y+= s.at(10); y+= s.at(5);
+	y += s.at(3); y+= s.at(9); y+= s.at(15); y+= s.at(2); y+= s.at(8); y+= s.at(14); y+= s.at(1); y+= s.at(7); y+= s.at(13); y+= s.at(0); y+= s.at(6); y+= s.at(12);
+	unsigned char x[22];
+	unsigned char j = 0;
+	//x[j++] = y.at(y.size()-1) & 0x3f;
+        for(unsigned char i = 15; i > 5; i--)
+        {
+		x[j] = (y.at(i) & 0x3f); j++;
+                x[j] = (y.at(i) >> 6) + ((y.at(i-1) & 0x0f) << 2); j++;
+		i--;
+		x[j] = (y.at(i) >> 4) + ((y.at(i-1) & 0x03) << 4); j++;
+		i--;
+		x[j] = (y.at(i) >> 2);j++;
+        }
 	std::string base64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	std::cout << base64.at(x[11]) << base64.at(x[4]) << base64.at(x[10]) << base64.at(x[5]) << base64.at(x[3]) << base64.at(x[9]) << base64.at(x[15]) << base64.at(x[2]) << base64.at(x[8]) << base64.at(x[14]) << base64.at(x[1]) << base64.at(x[7]) << base64.at(x[13]) << base64.at(x[0]) << base64.at(x[6]) << base64.at(x[12]) << std::endl;
+	std::cout << "base64 size: " << base64.size() << std::endl;
+	std::cout << "last 6 bits: " << base64.at((char)(s.at(s.size()-1 ) >> 2 )) << "\nfirst six bits: " << base64.at((char)(s.at(0) & 0x3f)) << std::endl;
+	std::cout << "first 3 characters: " << (int)(x[0]) << " " << (int)(x[1]) << " " << (int)(x[2]) << std::endl;
+	std::cout << base64.at(x[0]) << base64.at(x[1]) << base64.at(x[2]) << std::endl;
+	std::cout << "final string: " << print64(base64, x, 10) << std::endl;
 	delete myWrapper;
 
 	return 0;
@@ -95,4 +129,11 @@ std::string appInt(std::string pass, int length) {
 	}*/
 	std::cout << "appInt output: " << output << std::endl;
 	return output;
+}
+std::string print64(std::string base, unsigned char inc[], int incsize)
+{
+  for(unsigned char i = 0; i < incsize; i++)
+  {	if(inc[i] > 63) std::cout << "\n" << (int)inc[i] << std::endl;
+	else  std::cout << base.at(inc[i]);
+  }
 }
