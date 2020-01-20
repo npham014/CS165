57763236c27a55803b7a3df843008fa62783e0e8
Raymond <rchle001@ucr.edu>
2020-01-19 17:23:09 -0800

    May need to change the header includes and the note:

--- project1/Project1.cpp
+++ project1/Project1.cpp
@@ -1,13 +1,13 @@
-#include <hashlibpp.h>
+#include "../hashlib2plus/trunk/src/hashlibpp.h"
 #include <string>
 #include <sstream>
 #include <iostream>
-//note: to compile:  g++ -Ihashlib2plus/trunk/src/ -std=c++11 hashlib2plus/trunk/src/hl_md5wrapper.cpp hashlib2plus/trunk/src/hl_md5.cpp test.cpp
+//note: to compile:  g++ -I../hashlib2plus/trunk/src/ -std=c++11 ../hashlib2plus/trunk/src/hl_md5wrapper.cpp ../hashlib2plus/trunk/src/hl_md5.cpp Project1.cpp
 std::string convertHash(std::string hash, int length);
 std::string appInt(std::string pass, int length);
 
+
 int main(int argc, char** argv) {
-	
 	std::string salt = "hfT7jp2q";
 
 	std::string password = "zhgnnd"; //For testing hash, this will be replaced with a random password later	
@@ -19,15 +19,28 @@ int main(int argc, char** argv) {
 	hashwrapper *myWrapper = new md5wrapper();
 	
 	myWrapper->test();
+	std::cout << "altsum before hashing: " << altSum << std::endl;
 	altSum = myWrapper->getHashFromString(altSum);
+	std::cout << "altsum after hashing: ";
 	std::cout << altSum << std::endl;
 		
 
 	std::string interm0 = password + "$1$" + salt;
-	interm0 += convertHash(myWrapper->getHashFromString(altSum), password.length());
+	interm0 += convertHash((altSum), password.length());
 	std::cout << interm0 << std::endl;
 	interm0 += appInt(password, password.length());
-	std::cout << interm0 << std::endl;
+	std::cout << "intermediate0 before hashing: " <<  interm0 << std::endl;
+	std::cout << "intermediate0 after hashing: " << myWrapper->getHashFromString(interm0) << std::endl << std::endl;
+	std::string intermi, intermiprehash;
+	/*for(unsigned int i = 0; i <= 999; i++) // intermediate1000 loop, incomplete
+	{
+		if(i%2) intermiprehash += password;
+		else 
+		{
+			if(i == 0)
+			
+		}
+	}*/
 	delete myWrapper;
 
 	return 0;
@@ -45,20 +58,27 @@ std::string convertHash(std::string hash, int length) {
 	for(int j = 0; j < length; j++) {
 		output.push_back(temp.at(j));
 	}
+	std::cout << "convHash output: " << output << std::endl;
 	return output;
 }
 
 std::string appInt(std::string pass, int length) {
 	std::string output;
-	while(length > 0) {
+	std::string temp = pass;
+	while((length > 0)) {
 		if(length % 2) {
-			output.push_back('\0');
+			output+='\0';
 		}
 		else {
-			output.push_back(pass.at(pass.length() - 1));
+			output+= pass.at(0);
 		}
 		
 		length = length >> 1;
 	}
+	/*for(int i = 0; i < length; i+=2)
+	{
+		output.push_back(std::stoul(temp.substr(i,2), nullptr, 16));
+	}*/
+	std::cout << "appInt output: " << output << std::endl;
 	return output;
 }
